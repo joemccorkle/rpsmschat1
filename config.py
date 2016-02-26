31c4b94fc7b188843edc895fd4fe0c66da558b46
Kevin Krier <kkrier@twilio.com>
2016-02-26 12:20:00 -0800

    1

--- config.py
+++ config.py
@@ -27,3 +27,8 @@ TWILIO_API_KEY = os.environ['TWILIO_API_KEY']
 TWILIO_API_SECRET = os.environ['TWILIO_API_SECRET']
 TWILIO_IPM_SERVICE_SID = os.environ['TWILIO_IPM_SERVICE_SID']
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'postgres://jakigihvpfvuon:f-Rjo0Ljw1vLBu1tdMLqj-nJ1X@ec2-54-83-202-218.compute-1.amazonaws.com:5432/da3cp9a4fcb0pk'
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
